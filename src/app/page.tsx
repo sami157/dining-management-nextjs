56a65c231a1e94c493326df4bca58544e6e93e28
sami157 <sami.eee157@gmail.com>
2026-03-10 23:21:24 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      NextJS App Cooking
+      <div className="flex flex-col items-center">
+        NextJS App Cooking
+        <Button variant='default'
+        className="w-fit"
+        >
+          Button
+        </Button>
+      </div>
     </div>
   );
 }
